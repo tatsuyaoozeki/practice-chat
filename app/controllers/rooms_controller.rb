80d8b999d90a35b9d4e75ef2af5aa494c0a9fc3f
tatsuya oozeki <tasuya6281@icloud.com>
2019-12-04 13:01:18 +0900

    Add: model <--- has_many/belogs_to, room/controller <--- index/create/show

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,16 @@
 class RoomsController < ApplicationController
+  def index
+    @rooms = Room.all
+  end
+
+  def create
+    @room = Room.new
+    @room.save
+    redirect_to rooms_path
+  end
+
   def show
+    @room = Room.find(params[:id])
+    @message = @room.messages.recent.limit(5).reverse
   end
 end
